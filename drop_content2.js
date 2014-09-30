ef17ef55503a35a67a5cc0aad34a9797469f11e4
arvpan <arvpan@users.noreply.github.com>
2014-09-30 13:47:46 +0530

    Create drop_content2.js

---
+++ drop_content2.js
@@ -0,0 +1,5 @@
+var tipuedrop = {"pages": [
+     {"title": "VBA Search", "thumb": "", "text": "VBA Search, VBA Excel", "tags": "VBA, Macro, Learning ", "loc": "#"},
+     {"title": "VBA Search", "thumb": "", "text": "VBA Search, VBA Excel", "tags": "VBA, Macro, Learning ", "loc": "#"},
+     {"title": "VBA Search", "thumb": "", "text": "VBA Search, VBA Excel", "tags": "VBA, Macro, Learning ", "loc": "#"}
+]};
